66d83d7f69c8f31d98a0f1bdd37fd6f80a345fdc
MoustaphaTall <65893882+MoustaphaTall@users.noreply.github.com>
2020-07-05 23:09:41 +0200

    complete

--- sidekick/js/main.js
+++ sidekick/js/main.js
@@ -1,4 +1,16 @@
 $(function () {
-    $('.counter').counterUp();
+    $('.counter').counterUp({
+        time: 1500
+    });
     new WOW().init();
+
+    var firstModal = $('#firstmodal');
+    var rickroll = $('.rickroll');
+    firstModal.on('hide.bs.modal', function () {
+        rickroll.trigger('pause');
+    });
+
+    var scroll = new SmoothScroll('a[href*="#"]', {
+        speed: 200
+    });
 });
